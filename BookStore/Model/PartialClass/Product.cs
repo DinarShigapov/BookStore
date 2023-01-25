74bfd1f7e7c32d9ae6356a060b2b62cb99047bd5
DinarShigapov <dinarhertz@gmail.com>
2023-01-25 20:36:42 +0300

    Beta v1

--- BookStore/Model/PartialClass/Product.cs
+++ BookStore/Model/PartialClass/Product.cs
@@ -21,15 +21,70 @@ namespace BookStore.Model
                 Image img = new Image();
                 if (Image != null)
                 {
-                    img.Source = new BitmapImage(new Uri("/Resources/" + Image, UriKind.Relative));
+                    img.Source = new BitmapImage(new Uri("/Resources/Material/" + Image, UriKind.Relative));
                 }
                 else
                 {
-                    img.Source = new BitmapImage(new Uri("/Resources/picture.png", UriKind.Relative));
+                    img.Source = new BitmapImage(new Uri("/Resources/DefaultImages/picture.png", UriKind.Relative));
                 }
                 return img.Source;
             }
         }
+
+        public string StrDiscount
+        {
+            get 
+            {
+                return $"{Discount * 100}%";
+            }
+        }
+        
+        public string BGDiscount
+        {
+            get 
+            {
+                if (Discount > 0.05)
+                    return "#7fff00";
+                else
+                    return $"{default(Color)}";
+            }
+        }
+
+        public decimal DiscountPrice
+        {
+            get
+            {
+                if (Discount == 0)
+                    return Math.Round(Price, 2);
+                else
+                    return Math.Round((decimal)Price - Convert.ToDecimal(Price) * Convert.ToDecimal(Discount), 2);
+
+            }
+        }
+
+        public TextDecorationCollection TDPrice
+        {
+            get 
+            {
+                if (Discount == 0)
+                    return default;
+                else
+                    return TextDecorations.Strikethrough;
+            }
+        }
+
+        public Visibility VDiscountPrice
+        {
+            get
+            {
+                if (Discount == 0)
+                    return Visibility.Collapsed;
+                else
+                    return Visibility.Visible;
+            }
+        }
+
+
 
 
     }
